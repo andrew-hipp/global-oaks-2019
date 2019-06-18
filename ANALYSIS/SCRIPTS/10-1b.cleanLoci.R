3f5f1810102daf479d009d94544ce06be7c5f0d4
Andrew Hipp <andrew.hipp@gmail.com>
2019-06-18 15:24:47 -0500

    Deleting old analyses etc. prior to final update

--- ANALYSIS/SCRIPTS/10-1b.cleanLoci.R
+++
@@ -1,8 +0,0 @@
-## funny story: I exported ALL the loci... only needed to export the ones we
-##   mapped. Fixing that now. In next round, only send the ones we need.
-
-tempFiles <- dir('allLocRax', patt = 'bestTree') %>%
-  gsub(pattern = 'RAxML_bestTree.', replacement = '', fixed = TRUE)
-
-removeFiles <- setdiff(tempFiles, oaks.map$uniques$name)
-writeLines(paste('rm RAxML_bestTree.', removeFiles, sep = ''), 'allLocRax/removeEm.sh')
